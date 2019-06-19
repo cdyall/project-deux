47bab0db88617f106e5aeef7522f3d8cf355c8a9
cdyall <cdyall1@gmail.com>
2019-06-18 20:13:37 -0400

    upload

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO POI(name) VALUES ('Meeses');
 INSERT INTO cats (name) VALUES ('Bobbi');
 INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
 INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
