1b661118b5f7f2b90a7a115319ef32762b437e57
Chris Magesty <cmagesty@gmail.com>
2019-07-02 17:16:10 -0400

    updates

---
+++ public/assets/Firebase/firebase.js
@@ -0,0 +1,15 @@
+// var fireBase = fireBase || firebase;
+// var hasInit = false;
+// var config = {
+//     apiKey: "AIzaSyAagydYAEaAUjk8bUFzvXi17bAwWQMh5G4",
+//     authDomain: "test-720bb.firebaseapp.com",
+//     databaseURL: "https://test-720bb.firebaseio.com",
+//     projectId: "test-720bb",
+//     storageBucket: "test-720bb.appspot.com",
+//     messagingSenderId: "32361078111",
+//     appId: "1:32361078111:web:8b8ba8912c3b2f3c"
+//   };
+// if(!hasInit){
+//     firebase.initializeApp(config);
+//     hasInit = true;
+// }
